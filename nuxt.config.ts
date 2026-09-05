c77d35ae158caeaf1ea5f62861d571064729688c
Brendan Murty <brendan@bcm.works>
2026-09-05 19:23:03 +1000

    Cleanup Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,41 @@
+// @ts-ignore-file
+//
+// Nuxt config - https://nuxt.com/docs/api/configuration/nuxt-config
+//
+
 import { fileURLToPath } from 'node:url'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  appConfig: {
+    appId: "job-mpf"
+  },
+  telemetry: false,
+  serverDir: "src/server",
+  srcDir: "src/app/",
   dir: {
-    "app": "src/app",
-    "assets": "src/app/assets",
-    "layouts": "src/app/layouts",
-    "middleware": "src/app/middleware",
-    "pages": "src/app/pages",
-    "plugins": "src/app/plugins",
     "public": "src/public",
     "shared": "src/shared",
+    "modules": "src/modules",
   },
-
-  serverDir: "src/server",
-
   nitro: {
     preset: "deno_server",
-    // @ts-ignore
     output: {
       dir: "dist"
     }
   },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
   ],
-
   devtools: {
-    enabled: true
+    enabled: false
   },
-
-  // @ts-ignore
-  css: ['~/src/app/assets/css/main.css'],
-
+  css: ['~/assets/css/main.css'],
   routeRules: {
     '/': { prerender: true }
   },
-
   compatibilityDate: '2026-06-30',
-
   icon: {
-    // Only bundle the icons the app actually uses rather than loading the full
-    // @iconify-json/simple-icons collection (4.8 MB, 3500+ icons) at build time.
     customCollections: [
       {
         prefix: 'simple-icons',
@@ -51,7 +43,6 @@ export default defineNuxtConfig({
       }
     ]
   },
-
   eslint: {
     config: {
       stylistic: {
